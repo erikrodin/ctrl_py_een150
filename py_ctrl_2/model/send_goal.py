0d86dac1099ea369703925d6a3cb01c2a3d759c4
erikrodin <45962044+erikrodin@users.noreply.github.com>
2023-11-16 14:04:59 +0100

    Update send_goal.py

--- py_ctrl_2/model/send_goal.py
+++ py_ctrl_2/model/send_goal.py
@@ -1,1 +1,38 @@
+import rclpy
+import json
+from std_msgs.msg import String
+import time
 
+def main(args=None):
+    rclpy.init(args=args)
+    node = rclpy.create_node('send_goal')  # type: ignore
+
+    publisher = node.create_publisher(String, '/set_state', 10)
+
+    msg = String()
+    pos = {"replan": False}
+
+    msg.data = json.dumps(pos)
+    publisher.publish(msg)
+
+    time.sleep(0.2)
+
+    # set a state here to force replan
+    pos = {
+        "replan": True, 
+        # change the goal here
+        "goal_as_string": "robot_pose == above_buffer && red_cube_at == pose_1 && green_cube_at == pose_3 && blue_cube_at == pose_2"
+    
+        #"robot_pose == "above_buffer && red_cube_at == buffer"
+        #"red_cube_at == pose_3 && green_cube_at == pose_2 && blue_cube_at == pose_1 && robot_pose == above_buffer"
+   }
+
+    msg.data = json.dumps(pos)
+    publisher.publish(msg)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
