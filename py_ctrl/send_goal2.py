8ef1960535891ffac8e6c664a3cd0f56938a2af0
erikrodin <45962044+erikrodin@users.noreply.github.com>
2023-11-16 14:07:38 +0100

    Add files via upload

---
+++ py_ctrl/send_goal2.py
@@ -0,0 +1,38 @@
+import rclpy
+import json
+from std_msgs.msg import String
+import time
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = rclpy.create_node('send_goal')  # type: ignore
+
+    publisher = node.create_publisher(String, '/set_state', 10)
+
+    msg = String()
+    pos = {"replan": False}
+
+    msg.data = json.dumps(pos)
+    publisher.publish(msg)
+
+    time.sleep(0.2)
+
+    # set a state here to force replan
+    pos = {
+        "replan": True, 
+        # change the goal here
+        "goal_as_string":"blue_cube_at == pose_2 && red_cube_at == r2_buffer && r1_robot_pose == above_r1_buffer" 
+        #"blue_cube_at == pose_1 && green_cube_at == r2_buffer && r1_robot_pose == above_r1_buffer"
+        #"blue_cube_at == r1_buffer && green_cube_at == r2_buffer"
+        ##"r1_robot_pose == above_r1_buffer && red_cube_at == r1_buffer && blue_cube_at == r2_buffer"
+    }
+
+    msg.data = json.dumps(pos)
+    publisher.publish(msg)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
